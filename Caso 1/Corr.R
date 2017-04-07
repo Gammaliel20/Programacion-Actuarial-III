56219609711f922df7d0cb82f0301617062d5781
Gammaliel20 <tavo_liel@hotmail.com>
2017-04-07 14:13:23 -0500

    Update Corr.R

--- Caso 1/Corr.R
+++ Caso 1/Corr.R
@@ -9,10 +9,10 @@ corr <- function(directorio, horizonte = 0){
     
     gamma3 <- numeric()
     
-    for(i in cat2) {
+    for(i in gamma2) {
         DIRF <- read.csv(paste(directorio, "/", formatC(i, width = 3, flag = "0",".csv"), sep = ""))
         gamma4 <- DIRF[complete.cases(DIRF), ]
         gamma3 <- c(gamma3, cor(gamma4$sulfate, gamma4$nitrate))
     }
-    return(cat3)
+    return(gamma3)
 }
